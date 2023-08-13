acf153900976c1bee319099e901d0ec2ef449b41
Anton Morozov <anomorch@gmail.com>
2023-08-13 17:43:54 +0300

    put all urls to one file

--- consts.php
+++ consts.php
@@ -1,6 +1,4 @@
 <?php
+
 const TOP_DIR = __DIR__;
 const COOKIE_NAME_OF_USER_ID = "user_id";
-const GUESTS_HOMEPAGE_ADDR = "";
-const CUSTOMER_HOMEPAGE_ADDR = "";
-const ADMIN_HOMEPAGE_ADDR = "";
---
+++ urls.php
@@ -0,0 +1,5 @@
+<?php
+
+const GUESTS_HOMEPAGE_ADDR = "/au/controller/common/login.php";
+const CUSTOMER_HOMEPAGE_ADDR = "";
+const ADMIN_HOMEPAGE_ADDR = "";
