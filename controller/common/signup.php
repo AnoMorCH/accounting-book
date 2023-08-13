b874d79210d7f6cb9ed5492b52f60e3b486ab2f3
Anton Morozov <anomorch@gmail.com>
2023-08-13 10:47:33 +0300

    implement signup page

---
+++ controller/common/signup.php
@@ -0,0 +1,39 @@
+<?php
+include_once "../../consts.php";
+include_once TOP_DIR . "/class/access.php";
+include_once TOP_DIR . "/class/user_handler.php";
+include_once TOP_DIR . "/enum/user_position.php";
+include_once TOP_DIR . "/helper.php";
+
+/**
+ * Обработать контроллер по демонстрации содержимого шаблона login.html.
+ */
+function signup(): string 
+{
+    $access = new Access(UserPosition::Guest->value);
+    $access->redirectUserToHisHomepageIfNeeded();
+    $template_path = TOP_DIR . "/view/common/signup.html";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        try {
+            (new UserHandler())->signup(
+                $_POST["email"],
+                $_POST["password"],
+                $_POST["first-name"],
+                $_POST["last-name"]
+            );
+            $context = [
+                "user_created_successfully" => "Вы успешно зарегистрировались"
+            ];
+        } catch (Exception $exception) {
+            $context = [
+                "user_hasnt_been_created" => $exception->getMessage()
+            ];
+        }
+        return get_html($template_path, $context);
+    }
+
+    return get_html($template_path);
+}
+
+echo signup();
