be4c976b7b626ca10ff52db9059430e06a738248
Anton Morozov <anomorch@gmail.com>
2023-08-16 10:32:58 +0300

    DRY urls.php

--- urls.php
+++ urls.php
@@ -1,22 +1,30 @@
 <?php
 
-// TODO. Think about drying all the links below.
-const URLS = [
+// Секции для построения URL-ссылок.
+$subfolder = "au";
+$controller_folder = "controller";
+$unauthorized_user_folder = "common";
+$customer_folder = "customer";
+$review_folder = "review";
+
+$urls = [
     // Страницы неавторизованного пользователя.
-    "guest_homepage" => "/au/controller/common/login.php",
-    "login_page" => "/au/controller/common/login.php",
-    "signup_page" => "/au/controller/common/signup.php",
-
+    "guest_homepage" => "/$subfolder/$controller_folder/$unauthorized_user_folder/login.php",
+    "login_page" => "/$subfolder/$controller_folder/$unauthorized_user_folder/login.php",
+    "signup_page" => "/$subfolder/$controller_folder/$unauthorized_user_folder/signup.php",
+    
     // Страницы клиента.
-    "customer_homepage" => "/au/controller/customer/review/mine-list.php",
-    "this_review_page" => "/au/controller/customer/review/index.php",
-    "my_reviews_page" => "/au/controller/customer/review/mine-list.php",
-    "public_reviews_page" => "/au/controller/customer/review/public-list.php",
-    "create_review_page" => "/au/controller/customer/review/create.php",
-
+    "customer_homepage" => "/$subfolder/$controller_folder/$customer_folder/$review_folder/mine-list.php",
+    "this_review_page" => "/$subfolder/$controller_folder/$customer_folder/$review_folder/index.php",
+    "my_reviews_page" => "/$subfolder/$controller_folder/$customer_folder/$review_folder/mine-list.php",
+    "public_reviews_page" => "/$subfolder/$controller_folder/$customer_folder/$review_folder/public-list.php",
+    "create_review_page" => "/$subfolder/$controller_folder/$customer_folder/$review_folder/create.php",
+    
     // Страницы администратора.
     "admin_homepage" => "",
-
+    
     // Общие страницы авторизованных пользователей.
-    "logout_page" => "/au/controller/common/logout.php",
+    "logout_page" => "/$subfolder/$controller_folder/$unauthorized_user_folder/logout.php",
 ];
+
+define("URLS", $urls);
