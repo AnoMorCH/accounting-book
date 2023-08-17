a88869ace8d284a3c5a48c25ab80ecfdf5b83d7f
Anton Morozov <anomorch@gmail.com>
2023-08-17 10:07:19 +0300

    make admin navbar as php

---
+++ navbar/admin.php
@@ -0,0 +1,14 @@
+<?php
+echo "
+	<header>
+		<a href='#'>Санаторий</a>
+	</header>
+	<input type='checkbox' id='nav' hidden checked />
+	<div overflow>
+		<ul>
+			<li><a href='#'></a>Отзывы на проверку</a></li>
+			<li><a href='#'>Все отзывы</a></li>
+			<li><a href='#'>Выход</a></li>
+		</ul>
+	</div>
+";
